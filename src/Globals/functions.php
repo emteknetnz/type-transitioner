3235f87c81c62e122aa3a6ab6f9f702e87a7f0fc
Steve Boyd <emteknetnz@gmail.com>
2022-03-11 12:20:27 +1300

    Detect types

--- src/Globals/functions.php
+++ src/Globals/functions.php
@@ -3,6 +3,9 @@
 use emteknetnz\TypeTransitioner\Config\Config;
 use emteknetnz\TypeTransitioner\Exceptions\TypeException;
 
+global $_tt_method_cache;
+$_tt_method_cache = [];
+
 /**
  * Note: mixed static types are only availabe from php8.0
  * 
@@ -10,49 +13,99 @@ use emteknetnz\TypeTransitioner\Exceptions\TypeException;
  * @param string $type
  * @return mixed
  */
-function _a($arg, string $type = 'unknown')
+// function _a(string $castType, &$_arg)
+function _a()
 {
-    $NO_TYPE = '(none)';
+    global $_tt_method_cache;
+
+    $outdir = str_replace('//', '/', BASE_PATH . '/artifacts');
+    if (!file_exists($outdir)) {
+        mkdir($outdir);
+    }
+    $outpath = "$outdir/tt.txt";
+
+    $TYPE_DYNAMIC = 'type-dynamic'; // php7 type is blank
+    $TYPE_MISSING = 'type-missing'; // dockblock return type is missing
+    $WHERE_MISSING = 'where-missing'; // php7 type is blank and docblock type is missing
 
     $d = debug_backtrace(0, 2);
     $file = $d[0]['file'] ?? '';
     $line = $d[0]['line'] ?? '';
     $class = $d[1]['class'] ?? '';
-    $type = $d[1]['type'] ?? '';
+    $callType = $d[1]['type'] ?? '';
     $method = $d[1]['function'] ?? '';
-    $args = $d[0]['args'] ?? [];
-    print_r([$file, $line, $class, $type, $method, $args]);
+    $args = $d[1]['args'] ?? [];
+    print_r([$file, $line, $class, $callType, $method, $args]);
 
     if (!$class || !$method) {
         echo 'No class or method';die;
     }
 
-    // https://www.php.net/manual/en/book.reflection.php
-    $refClass = new ReflectionClass($class);
-    $refMethod = $refClass->getMethod($method);
-    
-    $refDocblock = $refMethod->getDocComment();
-    $refParams = $refMethod->getParameters();
-    $refReturn = $refMethod->getReturnType();
+    $key = $class . '::' . $method;
+    if (!array_key_exists($key, $_tt_method_cache)) {
+        // https://www.php.net/manual/en/book.reflection.php
+        $reflClass = new ReflectionClass($class);
+        $reflMethod = $reflClass->getMethod($method);
+        
+        $reflDocblock = $reflMethod->getDocComment();
+        $reflParams = $reflMethod->getParameters();
+        $reflReturn = $reflMethod->getReturnType();
 
-    preg_match_all('#@param +([^ ]+)[ \t]+((?:\$)[A-Za-z0-9]+)#', $refDocblock, $m);
+        preg_match_all('#@param +([^ ]+)[ \t]+((?:\$)[A-Za-z0-9]+)#', $reflDocblock, $m);
+        $docblockParams = array_combine($m[2], $m[1]);
+        $methodParams = array_combine(
+            array_map(fn($p) => '$' . $p->getName(), $reflParams),
+            array_map(fn($p) => $p->hasType() ? $p->getType()->getName() : $TYPE_DYNAMIC, $reflParams)
+        );
+        
+        preg_match('#@return ([^ ]+)#', $reflDocblock, $m);
+        $docblockReturn = $m[1] ?? $TYPE_MISSING;
+        $methodReturn = $reflReturn ? $reflReturn->getName() : '';
 
-    $docNames = $m[2];
-    $docTypes = $m[1];
-    $actualNames = array_map(fn($p) => '$' . $p->getName(), $refParams);
-    $actualTypes = array_map(fn($p) => $p->hasType() ? $p->getType()->getName() : $NO_TYPE, $refParams);
-    $docParams = array_combine($docNames, $docTypes);
-    $actualParams = array_combine($actualNames, $actualTypes);
+        $_tt_method_cache[$key] = [
+            'docblockParams' => $docblockParams,
+            'methodParams' => $methodParams,
+            'docblockReturn' => $docblockReturn,
+            'methodReturn' => $methodReturn,
+        ];
+    }
+    $o = $_tt_method_cache[$key];
+    $vars = array_keys($o['methodParams']);
+    for ($i = 0; $i < count($vars); $i++) {
+        $var = $vars[$i];
+        $arg = $args[$i];
+        $paramType = $o['methodParams'][$var];
+        $paramWhere = 'method';
+        if ($paramType == $TYPE_DYNAMIC) {
+            if (array_key_exists($var, $o['docblockParams'])) {
+                $paramType = $o['docblockParams'][$var];
+                $paramWhere = 'docblock';
+            } else {
+                $paramWhere = $WHERE_MISSING;
+            }
+        }
+        if (is_null($arg)) {
+            $argType = 'null';
+        } elseif (is_object($arg)) {
+            $argType = get_class($arg);
+        } elseif (is_string($arg)) {
+            $argType = 'string';
+        } elseif (is_int($arg)) {
+            $argType = 'int';
+        } elseif (is_float($arg)) {
+            $argType = 'float';
+        } elseif (is_array($arg)) {
+            $argType = 'array';
+        } else {
+            $argType = 'unknown';
+        }
+        if ($argType == $paramType) {
+            continue;
+        }
+        $line = implode(',', [$file, $line, $class, $method, $var, $paramWhere, $paramType, $argType, $arg]);
+        file_put_contents($outpath, "$line\n", FILE_APPEND);
+    }
 
-    preg_match('#@return ([^ ]+)#', $refDocblock, $m);
-    $docReturn = $m[1] ?? '';
-
-    print_r([
-        'docParams' => $docParams,
-        'actualParams' => $actualParams,
-        'docReturn' => $docReturn,
-        'actualReturn' => $refReturn ? $refReturn->getName() : '',
-    ]);
     die;
 
     // TODO: get call stack so can provide useful exception data
