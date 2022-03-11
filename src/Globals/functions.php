d832acba5bc546764b50f47a3b33b2b9a1f74e52
Steve Boyd <emteknetnz@gmail.com>
2022-03-11 16:08:35 +1300

    Scan methods

--- src/Globals/functions.php
+++ src/Globals/functions.php
@@ -5,6 +5,82 @@ use emteknetnz\TypeTransitioner\Exceptions\TypeException;
 use SilverStripe\Core\ClassInfo;
 use SilverStripe\Core\Manifest\ClassManifest;
 
+function _scan_methods()
+{
+    $out = shell_exec('find ' . BASE_PATH . '/vendor/silverstripe/framework | grep .php$');
+    $paths = explode("\n", $out);
+    foreach ($paths as $path) {
+        if (strpos($path, '/src/') === false && strpos($path, '/code/') === false) {
+            continue;
+        }
+        if (strpos($path, '/tests/') !== false) {
+            continue;
+        }
+        $contents = file_get_contents($path);
+        $namespace = '';
+        if (preg_match('#[\r\n]namespace (.+?);#', $contents, $m)) {
+            $namespace = $m[1];
+        }
+        $class = '';
+        if (preg_match('#[\r\n]class (.+?)[ \r\n]#', $contents, $m)) {
+            $class = $m[1];
+        } else {
+            echo "Could not find class for path $path\n";
+            continue;
+        }
+        $fqcn = "$namespace\\$class";
+        $reflClass = new ReflectionClass($fqcn);
+        $methodData = [];
+        foreach ($reflClass->getMethods() as $reflMethod) {
+            $name = $reflMethod->getName();
+            // ReflectionClass::getMethods() sorts the methods by class (lowest in the inheritance tree first)
+            // so as soon as we find an inherited method not in the $contents, we can break
+            if (strpos($contents, "function $name") === false) {
+                break;
+            }
+            $methodData[] = _ett_get_method_data($reflClass, $reflMethod);
+        }
+        // only include methods with dynamic params or a dynamic return type
+        $methodData = array_filter($methodData, function (array $o) {
+            $hasDynamicParams = !empty(array_filter($o['methodParams'], fn(string $type) => $type == 'dynamic'));
+            return $hasDynamicParams || $o['methodReturn'] == 'dynamic';
+        });
+        print_r($methodData);
+        die;
+    }
+    die;
+}
+
+// This should be a static method on a utility class or similar
+function _ett_get_method_data(ReflectionClass $reflClass, ReflectionMethod $reflMethod)
+{
+    // https://www.php.net/manual/en/book.reflection.php
+    $reflDocblock = $reflMethod->getDocComment();
+    $reflParams = $reflMethod->getParameters();
+    $reflReturn = $reflMethod->getReturnType();
+
+    preg_match_all('#@param +([^ ]+)[ \t]+((?:\$)[A-Za-z0-9]+)#', $reflDocblock, $m);
+    $docblockParams = array_combine($m[2], $m[1]);
+    $methodParams = array_combine(
+        array_map(fn($p) => '$' . $p->getName(), $reflParams),
+        array_map(fn($p) => $p->hasType() ? $p->getType()->getName() : 'dynamic', $reflParams)
+    );
+    
+    preg_match('#@return ([^ ]+)#', $reflDocblock, $m);
+    $docblockReturn = $m[1] ?? 'missing';
+    $methodReturn = $reflReturn ? $reflReturn->getName() : 'dynamic';
+
+    return [
+        'namespace' => $reflClass->getNamespaceName(),
+        'class' => $reflClass->getName(),
+        'method' => $reflMethod->getName(),
+        'docblockParams' => $docblockParams,
+        'methodParams' => $methodParams,
+        'docblockReturn' => $docblockReturn,
+        'methodReturn' => $methodReturn,
+    ];
+}
+
 global $_ett_method_cache;
 $_ett_method_cache = [];
 
@@ -60,33 +136,9 @@ function _a()
     if (!array_key_exists($key, $_ett_method_cache)) {
 
         try {
-
-            // https://www.php.net/manual/en/book.reflection.php
             $reflClass = new ReflectionClass($calledClass);
             $reflMethod = $reflClass->getMethod($calledMethod);
-            
-            $reflDocblock = $reflMethod->getDocComment();
-            $reflParams = $reflMethod->getParameters();
-            $reflReturn = $reflMethod->getReturnType();
-
-            preg_match_all('#@param +([^ ]+)[ \t]+((?:\$)[A-Za-z0-9]+)#', $reflDocblock, $m);
-            $docblockParams = array_combine($m[2], $m[1]);
-            $methodParams = array_combine(
-                array_map(fn($p) => '$' . $p->getName(), $reflParams),
-                array_map(fn($p) => $p->hasType() ? $p->getType()->getName() : $TYPE_DYNAMIC, $reflParams)
-            );
-            
-            preg_match('#@return ([^ ]+)#', $reflDocblock, $m);
-            $docblockReturn = $m[1] ?? $TYPE_MISSING;
-            $methodReturn = $reflReturn ? $reflReturn->getName() : '';
-
-            $_tt_method_cache[$key] = [
-                'namespace' => $reflClass->getNamespaceName(),
-                'docblockParams' => $docblockParams,
-                'methodParams' => $methodParams,
-                'docblockReturn' => $docblockReturn,
-                'methodReturn' => $methodReturn,
-            ];
+            $_tt_method_cache[$key] = _ett_get_method_data($reflClass, $reflMethod);
         } catch (Exception $e) {
             echo "Exception when doing Refelction\n";
             return;
@@ -96,7 +148,7 @@ function _a()
     $vars = array_keys($o['methodParams']);
     for ($i = 0; $i < count($vars); $i++) {
         $var = $vars[$i];
-        $arg = $args[$i];
+        $arg = $args[$i] ?? null;
         $paramType = $o['methodParams'][$var];
         if ($paramType != $TYPE_DYNAMIC) {
             // strongly typed method params are a non-issue
@@ -147,21 +199,21 @@ function _a()
         file_put_contents($outpath, "$line\n", FILE_APPEND);
     }
 
-    return;
+    // return;
 
-    // TODO: get call stack so can provide useful exception data
-    $config = Config::inst();
-    if ($config->get(Config::LOG)) {
+    // // TODO: get call stack so can provide useful exception data
+    // $config = Config::inst();
+    // if ($config->get(Config::LOG)) {
 
-    }
-    if ($config->get(Config::THROW_EXCEPTION)) {
-        if ($type === 'string' && !is_string($arg)) {
-            throw new TypeException(sprintf('%s is not a string', $arg));
-        }
-    }
-    if ($config->get(Config::CAST)) {
-        if ($type === 'string') {
-            return (string) $arg;
-        }
-    }
+    // }
+    // if ($config->get(Config::THROW_EXCEPTION)) {
+    //     if ($type === 'string' && !is_string($arg)) {
+    //         throw new TypeException(sprintf('%s is not a string', $arg));
+    //     }
+    // }
+    // if ($config->get(Config::CAST)) {
+    //     if ($type === 'string') {
+    //         return (string) $arg;
+    //     }
+    // }
 }
