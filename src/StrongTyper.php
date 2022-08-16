aa84600c0da19bb1cbfc980a602d448bdd6339a1
Steve Boyd <emteknetnz@gmail.com>
2022-08-17 11:51:07 +1200

    .

--- src/StrongTyper.php
+++ src/StrongTyper.php
@@ -21,38 +21,89 @@ class StrongTyper extends Singleton
 {
     public function codeWrite()
     {
+        //         foreach (array_keys($combined[$fqcn]) as $methodName) {
         $combined = TraceResults::getInstance()->read();
         foreach (array_keys($combined) as $fqcn) {
             foreach (array_keys($combined[$fqcn]) as $methodName) {
-                $reflClass = new \ReflectionClass($fqcn);
-                $filename = $reflClass->getFileName();
-                if (strpos($filename, 'vendor/silverstripe/framework/') === false) {
+                // log($fqcn . '::' . $methodName);
+            }
+            $reflClass = new \ReflectionClass($fqcn);
+            $filename = $reflClass->getFileName();
+            if (strpos($filename, 'vendor/silverstripe/framework/') === false) {
+                continue;
+            }
+            if (strpos($filename, '/thirdparty/') !== false) {
+                continue;
+            }
+            $code = file_get_contents($filename);
+            $ast = $this->getAst($code);
+            $classes = $this->getClasses($ast);
+            if (empty($classes)) {
+                $classes = ['nonclass'];
+            }
+            // reverse methods so 'updating from the bottom' so that character offests remain correct
+            $classes = array_reverse($classes);
+            foreach ($classes as $class) {
+                if ($class == 'nonclass') {
                     continue;
                 }
-                if (strpos($filename, '/thirdparty/') !== false) {
-                    continue;
-                }
-                $code = file_get_contents($filename);
-                $ast = $this->getAst($code);
-                $classes = $this->getClasses($ast);
-                if (empty($classes)) {
-                    $classes = ['nonclass'];
-                }
-                // reverse methods so 'updating from the bottom' so that character offests remain correct
-                $classes = array_reverse($classes);
-                foreach ($classes as $class) {
-                    if ($class == 'nonclass') {
-                        $methods = ['nonclass'];
-                    } else {
-                        $methods = $this->getMethods($class);
+                //log($class->name);
+                $methods = $this->getMethods($class);
+                $methods = array_reverse($methods);
+                /** @var ClassMethod $method */
+                foreach ($methods as $method) {
+                    $methodName = $method->name->name;
+                    $res = $combined[$fqcn][$methodName];
+                    if (!$res['trace']['traced']) {
+                        continue;
                     }
-                    $methods = array_reverse($methods);
-                    /** @var ClassMethod $method */
-                    foreach ($methods as $method) {
-                        // $name = $method <<<<
-                    }
-                }
+                    // print_r($res);die;
+                    $start = $method->getStartFilePos();
+                    $end = $method->getEndFilePos();
+                    $methodStr = substr($code, $start, $end);
+                    preg_match('#(\(.+?{)#s', $methodStr, $m);
+                    $methodSig = $m[1];
+                    preg_match('#\((.*?)\)#s', $methodSig, $m);
+                    $paramSig = $m[1];
+                    preg_match('#(\).*?{)#s', $methodSig, $m);
+                    $returnSig = $m[1];
+                    foreach (preg_split('#, ?(?=\$)#', $paramSig) as $paramStr) {
+                        if (empty($paramStr)) {
+                            // no params
+                            continue;
+                        }
+                        preg_match('#(\$[a-zA-Z0-9_]+)#', $paramStr, $m);
+                        $paramName = $m[1];
+                        $strongType = $res['methodParamTypes'][$paramName];
+                        if ($strongType != 'DYNAMIC') {
+                            continue;
+                        }
+                        $flags = $res['methodParamFlags'][$paramName];
+                        if (!isset($res['trace']['results']['params'][$paramName]['argTypes'])) {
+                            // no trace results
+                            continue;
+                        }
+                        $argTypes = $res['trace']['results']['params'][$paramName]['argTypes'];
+                        $isOptional = ($flags & MethodAnalyser::ETT_OPTIONAL) == MethodAnalyser::ETT_OPTIONAL;
+                        if ($isOptional) {
+                            // treat optional argtype as if it was also traced
+                            preg_match('#= ?(.+$)#', $paramStr, $m);
+                            $optionalVal = eval($m[1] . ';');
+                            $optionalType = MethodAnalyser::getInstance()->getArgType($optionalVal);
+                            $argTypes[] = $optionalType;
+                            $argTypes = array_unique($argTypes);
+                        }
+                        $isReference = ($flags & MethodAnalyser::ETT_REFERENCE) == MethodAnalyser::ETT_REFERENCE;
+                        $isVariadic = ($flags & MethodAnalyser::ETT_VARIADIC) == MethodAnalyser::ETT_VARIADIC;
 
+
+                        //print_r($res);die;
+                        // log($param);
+                    }
+                    // print_r([$methodSig, $paramSig, $returnSig]);die;
+                    
+                    //log($class->name . '::' . $method->name);
+                }
             }
         }
         $this->printLog();
